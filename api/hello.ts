e261f5f8a314f923d50e3a7507a1ad8ac92f4db7
ltostes <lucasrmtostes@gmail.com>
2025-05-05 10:56:08 -0300

    backend cleanup

--- api/hello.ts
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
- 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const name = req.query.name ?? 'World';
-  res.writeHead(200);
-  res.write(`Hello ${name}!`);
-  res.end();
-}
